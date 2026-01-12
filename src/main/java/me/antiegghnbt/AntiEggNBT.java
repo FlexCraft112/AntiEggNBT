cf190e2519880576f5fd683be207603df8d4ee10
FlexCraft112 <159067201+FlexCraft112@users.noreply.github.com>
2026-01-13 00:24:35 +0100

    Update AntiEggNBT.java

--- src/main/java/me/antiegghnbt/AntiEggNBT.java
+++ src/main/java/me/antiegghnbt/AntiEggNBT.java
@@ -1,7 +1,6 @@
 package me.antiegghnbt;
 
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
@@ -9,6 +8,7 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.entity.Player;
 
 public class AntiEggNBT extends JavaPlugin implements Listener {
 
@@ -30,12 +30,13 @@ public class AntiEggNBT extends JavaPlugin implements Listener {
 
         ItemMeta meta = item.getItemMeta();
 
-        // ❌ Любой spawn egg с метой = NBT / кастом
+        // ❌ Любой spawn egg с метой (NBT / кастом)
         if (meta != null && (
                 meta.hasCustomModelData()
                 || meta.hasDisplayName()
                 || meta.hasLore()
         )) {
+
             event.setCancelled(true);
 
             Player p = event.getPlayer();
